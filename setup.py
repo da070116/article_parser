c33ff7099297097f9dde245b01ceae885eb06dd7
Alexander <da070116@gmail.com>
2020-06-16 17:46:03 +0300

    Release 0.0.1
    All functionality (except SQL-file generation) is done.
    Code is commented and cleaned

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="py_lermont",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
+    author="Alexander Dubrovin",
+    author_email="da070116@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/da070116/py_lermont",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
